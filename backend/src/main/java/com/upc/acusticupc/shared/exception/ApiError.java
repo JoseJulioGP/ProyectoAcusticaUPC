b3665674ea9a9e7265ddcb7362873b371cc1cfa5
Mario Poveda <mnpoveda@unicesar.edu.co>
2026-05-21 17:08:10 -0500

    feat(auth):creacion clase ApiError

---
+++ backend/src/main/java/com/upc/acusticupc/shared/exception/ApiError.java
@@ -0,0 +1,15 @@
+package com.upc.acusticupc.shared.exception;
+
+import java.time.OffsetDateTime;
+
+public record ApiError(
+    OffsetDateTime timestamp,
+    int status,
+    String error,
+    String message,
+    String path
+) {
+    public static ApiError of(int status, String error, String message, String path) {
+        return new ApiError(OffsetDateTime.now(), status, error, message, path);
+    }
+}
