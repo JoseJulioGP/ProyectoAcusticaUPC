cf697adce821e56629afbd07cde4e99e0542e908
Mario Poveda <mnpoveda@unicesar.edu.co>
2026-05-21 16:04:58 -0500

    feat(auth):creacion interface MeasurementBatchRepository

---
+++ backend/src/main/java/com/upc/acusticupc/sonometry/domain/repository/MeasurementBatchRepository.java
@@ -0,0 +1,15 @@
+package com.upc.acusticupc.sonometry.domain.repository;
+
+import com.upc.acusticupc.sonometry.domain.model.BatchStatus;
+import com.upc.acusticupc.sonometry.domain.model.MeasurementBatch;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface MeasurementBatchRepository extends JpaRepository<MeasurementBatch, UUID> {
+
+    List<MeasurementBatch> findByStatusOrderByUploadedAtDesc(BatchStatus status);
+}
